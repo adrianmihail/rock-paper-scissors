f38081f4be46d632663f21aa55388af167eb1f90
adrianmihail <adrianmihail00@gmail.com>
2023-12-23 13:50:54 +0200

    Add User UI

--- script.js
+++ script.js
@@ -1,46 +1,47 @@
-// Algorithm
-
-// 1. Initialize rock
+// get computer choice
 let rock = "Rock";
-
-// 2. Initialize paper
 let paper = "Paper";
-
-// 3. Initialize scissors
 let scissors = "Scissors";
-
-// 4. Initialize computer
 let computer;
-
-// 5. Initialize an array which contains rock, paper and scissors
 let toolsArr = [rock, paper, scissors];
-
-// 6. Function which calls the computer choice
-
-function getComputerChoice() {
-    // 7. Initialize a variable which randomly chooses a tool
-    let randomTool = Math.floor(Math.random() * toolsArr.length);
-
-    // 8. Asign random tool to computer
-    return computer = toolsArr[randomTool];
-}
-// 10. Ask user to choose rock, paper or scissors
-function getUserChoice() {
-    let userInput = prompt("Please choose between 'Rock', 'Paper' or 'Scissors'")
-
-    //11. Uppercase the first letter of the input
-    userInput = userInput.charAt(0).toUpperCase() + userInput.slice(1).toLowerCase();
-
-    return userInput;
-}
 
 // xx. Account for the score
 let computerScore = 0;
 let userScore = 0;
 
-function oneRound (playerSelection, computerSelection) {
-    computerSelection = getComputerChoice ();
-    playerSelection = getUserChoice();
+function getComputerChoice() { 
+    let randomTool = Math.floor(Math.random() * toolsArr.length);
+
+    return computer = toolsArr[randomTool];
+};
+
+// Play the game using buttons
+
+let playerButtonPress;
+
+const rockBtn = document.querySelector('#rock');
+rockBtn.addEventListener('click', () => {
+    playerButtonPress = rock;
+    oneRound();
+});
+
+const paperBtn = document.querySelector('#paper');
+paperBtn.addEventListener('click', () => {
+    playerButtonPress = paper;
+    oneRound();
+});
+
+const scissorsBtn = document.querySelector('#scissors');
+scissorsBtn.addEventListener('click', () => {
+    playerButtonPress = scissors;
+    oneRound();
+});
+
+// Play one round
+
+function oneRound () {
+    let playerSelection = playerButtonPress;
+    let computerSelection = getComputerChoice();
     // 12. Create switch - case depending on computer input
 
     switch (computerSelection) {
@@ -49,7 +50,6 @@ function oneRound (playerSelection, computerSelection) {
             switch (playerSelection) {
                 case rock :
                     console.log("You both chose Rock! Play again!")
-                    oneRound();
                 break;
 
                 case paper :
@@ -68,7 +68,6 @@ function oneRound (playerSelection, computerSelection) {
             switch (playerSelection) {
                 case paper :
                     console.log("You both choose Paper! Play again!")
-                    oneRound();
                 break;
 
                 case scissors :
@@ -86,7 +85,6 @@ function oneRound (playerSelection, computerSelection) {
             switch (playerSelection) {
                 case scissors :
                     console.log("You both choose Scissors! Play again!")
-                    oneRound();
                 break;
 
                 case rock :
@@ -101,22 +99,21 @@ function oneRound (playerSelection, computerSelection) {
         break;
     }
 
+    const userScoreboard = document.querySelector('#userScoreboard');
+    userScoreboard.textContent = `Your score: ${userScore}`;
+
+    const computerScoreboard = document.querySelector('#computerScoreboard');
+    computerScoreboard.textContent = `Computer score: ${computerScore}`;
+
+    let countScore = userScore + computerScore;
+    console.log(countScore);
+
+    if (countScore == 5){
+        if(userScore > computerScore){
+            alert("You won!");
+        } else{
+            alert('Computer won!');
+        };
+    };
 }
 
-// 15. Create function to play 5 rounds
-
-function game() {
-    for (let i = 1; i <= 5; ++i) {
-        oneRound(i);
-        console.log("Your Score: ", userScore);
-        console.log("Computer Score: ", computerScore);
-    }
-
-    if (userScore > computerScore){
-        return console.log("You Won!");
-    } else {
-        return console.log("Computer Won!");
-    }; 
-}
-
-game();
